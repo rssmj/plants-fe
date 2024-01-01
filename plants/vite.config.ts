bb797f480497e3cb2227e5af6dc03497cf396548
rssmj <58471626+rssmj@users.noreply.github.com>
2024-01-01 08:28:48 -0500

    update config

--- plants/vite.config.ts
+++ plants/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://main--gleaming-bubblegum-a0b73c.netlify.app/',
+  // base: 'https://main--gleaming-bubblegum-a0b73c.netlify.app/',
 });
